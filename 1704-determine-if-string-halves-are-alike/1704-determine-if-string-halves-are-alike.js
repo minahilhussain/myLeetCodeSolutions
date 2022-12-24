c03e9bc3465abb0bf7738c6954655bf5e7ca7551
Minahil Hussain <minahilh21@gmail.com>
2022-12-24 22:49:23 +0500

    Time: 72 ms (87.21%), Space: 43.5 MB (43.94%) - LeetHub

---
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var halvesAreAlike = function(s) {
+    s = s.toLowerCase();
+    let left = s.slice(0,s.length/2);
+    let right = s.slice(s.length/2);   
+    left = left.split('').filter((letter)=> letter == 'a'|| letter == 'e'|| letter == 'i' ||letter == 'o'||letter == 'u').join('');
+    right = right.split('').filter((letter)=> letter == 'a'|| letter == 'e'|| letter == 'i' ||letter == 'o'||letter == 'u').join('');
+    return left.length === right.length;
+};
