3391fe56796801a29443a85c860f6cc8e6095455
Minahil Hussain <minahilh21@gmail.com>
2023-01-10 10:30:52 +0500

    Time: 72 ms (44.22%), Space: 44.6 MB (11.06%) - LeetHub

---
+++ 2529-maximum-count-of-positive-integer-and-negative-integer/2529-maximum-count-of-positive-integer-and-negative-integer.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maximumCount = function(nums) {
+    return Math.max(
+      nums.filter((num) => num > 0).length,
+      nums.filter((num) => num < 0).length
+    );
+};
