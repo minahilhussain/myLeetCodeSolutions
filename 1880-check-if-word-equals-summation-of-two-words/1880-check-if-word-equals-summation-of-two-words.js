5c18633ce163eb1506d0a78ab952687d349711ce
Minahil Hussain <minahilh21@gmail.com>
2023-01-13 11:33:37 +0500

    Time: 60 ms (91.60%), Space: 41.3 MB (98.47%) - LeetHub

---
+++ 1880-check-if-word-equals-summation-of-two-words/1880-check-if-word-equals-summation-of-two-words.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} firstWord
+ * @param {string} secondWord
+ * @param {string} targetWord
+ * @return {boolean}
+ */
+var isSumEqual = function(firstWord, secondWord, targetWord) {
+    return getNumericValue(firstWord) + getNumericValue(secondWord) === getNumericValue(targetWord);
+};
+
+const getNumericValue = (str) => {
+    let sum = '';
+    for (let i = 0; i < str.length; i++) {
+        sum += str.charCodeAt(i) - 97
+    }
+    return Number(sum);
+}
