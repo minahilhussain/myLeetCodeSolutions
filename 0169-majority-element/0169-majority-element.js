88e3188fdd164b70646a1d68461ec1ceeb077057
Minahil Hussain <minahilh21@gmail.com>
2023-01-17 10:56:36 +0500

    Time: 80 ms (58.56%), Space: 44.2 MB (40.40%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+    let map = new Map();
+    let a = [];
+    for (let i = 0; i < nums.length; i++) {
+        map.set(nums[i], (map.get(nums[i]) || 0) + 1);
+    }
+    for (let [key, value] of map.entries()) {
+        a.push({ key: key, value: value });
+    }
+    return a.sort((a, b) => b.value - a.value)[0].key;
+};
