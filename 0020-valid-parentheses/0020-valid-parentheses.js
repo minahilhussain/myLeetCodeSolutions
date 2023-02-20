7e8be6eb3ac2923d5b1cd0d40aabca2df6b8bb62
Minahil Hussain <minahilh21@gmail.com>
2023-02-20 23:03:00 +0500

    Time: 75 ms (43.45%), Space: 41.8 MB (94.53%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.js
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -4,30 +4,18 @@
  */
 var isValid = function(s) {
     let stack = [];
-    let open = ['(','{','['];
-    let close = [')','}',']'];
-    
+
     for(let i = 0; i < s.length; i++){
-        if(open.indexOf(s[i]) !== -1) {
+        if(s[i] === "(" || s[i] === "{" || s[i] === "[") {
             stack.push(s[i]);
         } else {
             if (stack.length == 0) return false;
             let last = stack.pop();
-            if(!checkPair(s[i], last)) return false;
+            if(!(s[i] === ")" && last === "(" || 
+                 s[i] === "}" && last === "{" || 
+                 s[i] === "]" && last === "["  ))
+                 return false;
         }
     }
     return stack.length === 0;
 };
-
-function checkPair(str1, str2) {
-    if(str1 === ")") {
-        if(str2 === "(") return true;
-    }
-    if(str1 === "}") {
-        if(str2 === "{") return true;
-    }
-    if(str1 === "]") {
-        if(str2 === "[") return true;
-    }
-    return false;
-}
