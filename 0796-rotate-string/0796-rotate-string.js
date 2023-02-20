b59a574357bdc843314d0dc31be3aa9873d58d90
Minahil Hussain <minahilh21@gmail.com>
2023-02-20 11:00:10 +0500

    Time: 55 ms (95.25%), Space: 42.4 MB (34.79%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.js
+++ 0796-rotate-string/0796-rotate-string.js
@@ -5,9 +5,9 @@
  */
 var rotateString = function(raw, goal) {
     let oldStr = raw;
-    while(raw !== goal) {
+    for(let i = 0; i < raw.length; i++) {
         raw = `${raw.slice(1)}${raw.slice(0,1)}`;
-        if(raw === oldStr) return false;
+        if(raw === goal) return true;
     }
-    return true;
+    return false;;
 };
