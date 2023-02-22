87d5f47cda85985163233e9513912ac8fda5d70c
Minahil Hussain <minahilh21@gmail.com>
2023-02-22 10:29:55 +0500

    Time: 98 ms (16.00%), Space: 44.4 MB (20.00%) - LeetHub

--- 1408-string-matching-in-an-array/1408-string-matching-in-an-array.ts
+++ 1408-string-matching-in-an-array/1408-string-matching-in-an-array.ts
@@ -2,10 +2,11 @@ function stringMatching(words: string[]): string[] {
     let arr = [];
     for(let i = 0; i < words.length; i++) {
         for(let j = 0; j < words.length; j++) {
-            if(i !== j && words[i].includes(words[j])) {
-                arr.push(words[j]);
+            if(i !== j && words[j].includes(words[i])) {
+                arr.push(words[i]);
+                break;
             }
         }
     }
-    return [...new Set(arr)];
+    return arr;
 };
