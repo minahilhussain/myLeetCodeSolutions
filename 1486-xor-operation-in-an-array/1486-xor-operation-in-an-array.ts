1470dcfeb0fd505ea1a0e8f823bc0e66661e3c0e
Minahil Hussain <minahilh21@gmail.com>
2023-02-27 11:20:57 +0500

    Time: 60 ms (85.11%), Space: 42.7 MB (80.85%) - LeetHub

--- 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.ts
+++ 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.ts
@@ -1,5 +1,5 @@
 function xorOperation(n: number, start: number): number {
     let xor = start;
-    for(let i = 1; i < n; i++ ) xor ^= start + 2 * i
+    for(let i = 1; i < n; i++ ) xor ^= start + i * 2
     return xor;
 };
