3481311414fbff9c8bd37d984fd02990d716aeb0
Minahil Hussain <minahilh21@gmail.com>
2023-03-15 11:59:31 +0500

    Time: 79 ms (97.93%), Space: 53.4 MB (55.69%) - LeetHub

---
+++ 0896-monotonic-array/0896-monotonic-array.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var isMonotonic = (nums)=> {
+    if(nums.length <= 1) return true;
+    let flag = -1;
+    for(let i = 0; i < nums.length - 1; i++) {
+        if(nums[i] < nums[i+1]) {
+            flag = 0;
+            break;
+        } else if(nums[i] > nums[i+1]) {
+            flag = 1;
+            break;
+        }
+    }
+    if(flag === 0) {
+        for(let i = 0; i < nums.length - 1; i++) {
+            if (nums[i]>nums[i+1]) return false;
+        }
+    }
+    else if(flag === 1) {
+        for(let i = 0; i < nums.length - 1; i++) {
+            if (nums[i]<nums[i+1]) return false;
+        }
+    }
+    return true;
+};
