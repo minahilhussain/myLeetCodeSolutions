e378b766e25a7999b0eb92137769e270828459f5
Minahil Hussain <minahilh21@gmail.com>
2023-03-21 11:39:34 +0500

    Time: 116 ms (31.33%), Space: 47.1 MB (63.22%) - LeetHub

---
+++ 0002-add-two-numbers/0002-add-two-numbers.js
@@ -0,0 +1,68 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function(l1, l2) {
+    var sum = null;
+    if(!l1) return l2;
+    if(!l2) return l1;
+    var carry = 0;
+    while(l1 && l2) {
+        var number = l1.val +l2.val +carry;
+        if (number>9) {
+            carry = 1;
+            number = number%10;
+        } else {
+            carry = 0;
+        }
+        sum = insert(sum, number); 
+        l1 = l1.next;
+        l2 = l2.next;
+    }
+    while(l1) {
+       var number = l1.val + carry;
+        if (number>9) {
+            carry = 1;
+            number = number%10;
+        } else {
+            carry = 0;
+        }
+        sum = insert(sum, number);
+        l1 = l1.next; 
+    }
+    while(l2) {
+       var number = l2.val + carry;
+        if (number>9) {
+            carry = 1;
+            number = number%10;
+        } else {
+            carry = 0;
+        }
+        sum = insert(sum, number);
+        l2 = l2.next; 
+    }
+    if (carry === 1) sum = insert(sum, 1);
+    return sum;
+};
+var insert = (head, data) => {
+    var temp = new ListNode(data);
+    if (head === null) {
+        head = temp;
+        return head;
+    } else {
+        var cur = head;
+        while(cur.next !== null) {
+            cur = cur.next
+        }
+        cur.next = temp;
+        return head;
+    }
+}
