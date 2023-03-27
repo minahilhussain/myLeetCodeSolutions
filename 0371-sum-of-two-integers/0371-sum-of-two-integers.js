3b2f91df4f3abb9d5431f7671bbab01e8e7cb8a1
Minahil Hussain <minahilh21@gmail.com>
2023-03-27 10:04:03 +0500

    Time: 65 ms (30.04%), Space: 42.1 MB (20.75%) - LeetHub

---
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} a
+ * @param {number} b
+ * @return {number}
+ */
+var getSum = function(a, b) {
+    while(b !== 0) {
+        const carry = a & b;
+        a = a ^ b;
+        b = carry << 1;
+    }
+    return a; 
+};
