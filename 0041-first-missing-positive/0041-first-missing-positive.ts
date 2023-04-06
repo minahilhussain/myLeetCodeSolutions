a18bceef4ae06bb51f1c770b30d5c6bfc6bf4bbe
Minahil Hussain <minahilh21@gmail.com>
2023-04-06 10:49:04 +0500

    Time: 105 ms (34.43%), Space: 64.9 MB (5.49%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.ts
+++ 0041-first-missing-positive/0041-first-missing-positive.ts
@@ -1,9 +1,9 @@
 function firstMissingPositive(num: number[]): number {
-    const nums = new Set(num.sort((a,b)=>a-b));
+    const nums = new Set(num);
     const max = Math.max(...nums);
-    if(max < 0) return 1;
-    for(let i = 1; i < max; i++) {
+    if (max < 0) return 1;
+    for (let i = 1; i < max; i++) {
         if(!nums.has(i)) return i;
     }
-    return max+1;
+    return max + 1;
 };
