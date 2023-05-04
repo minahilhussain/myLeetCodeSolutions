f357a01770beb4a883e32a371bdea923b3da2d9a
Minahil Hussain <minahilh21@gmail.com>
2023-05-04 17:28:13 +0500

    Time: 63 ms (65.22%), Space: 44.6 MB (36.23%) - LeetHub

---
+++ 1051-height-checker/1051-height-checker.ts
@@ -0,0 +1,9 @@
+function heightChecker(heights: number[]): number {
+    let raw = heights.slice(0);
+    heights.sort((a,b)=> a-b);
+    let count = 0;
+    for(let i = 0; i < heights.length; i++) {
+        if(heights[i] !== raw[i]) count++;
+    }
+    return count;
+};
