5cc5f70a0314432aa2d85153357942247ef34aca
Minahil Hussain <minahilh21@gmail.com>
2023-07-13 15:07:03 +0500

    Time: 76 ms (56.49%), Space: 45.5 MB (57.14%) - LeetHub

--- 2710-remove-trailing-zeros-from-a-string/2710-remove-trailing-zeros-from-a-string.ts
+++ 2710-remove-trailing-zeros-from-a-string/2710-remove-trailing-zeros-from-a-string.ts
@@ -2,6 +2,7 @@ function removeTrailingZeros(num: string): string {
     if(num[ num.length-1 ] !== '0')  return num;
     else {
         let flag: number = 0;
+        
         for(let i = num.length - 1; i >= 0; i--) {
             if (num[i] === '0') flag++;
             else break;
