0f9b3a295c07a071eef804bd62f27fb2aba0bec0
Minahil Hussain <minahilh21@gmail.com>
2023-09-06 14:59:23 +0500

    Time: 154 ms (10.31%), Space: 48.6 MB (13.38%) - LeetHub

---
+++ 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.js
@@ -0,0 +1,49 @@
+/**
+ * @param {number[]} num
+ * @param {number} k
+ * @return {number[]}
+ */
+var addToArrayForm = function(num, d) {
+    let k = d.toString().split('');
+    let size1 = num.length;
+    let size2 = k.length;
+    if (size1 > size2) {
+        let dif = size1 - size2;
+        for(let i = 0; i < dif; i++) {
+            k.unshift(0);
+        }
+    } 
+    if (size1 < size2) {
+        console.log(size1, size2);
+        let dif = size2 - size1;
+        for(let i = 0; i < dif; i++) {
+            num.unshift(0);
+        }
+    } 
+    let carry = 0;
+    for(let i = num.length - 1; i >= 0; i--) {
+        let sum = num[i] + Number(k[i]);
+        if(sum > 9) {
+            sum = sum % 10;
+            num[i] = sum + carry;
+            if(num[i] === 10) {
+                num[i] = 0;
+            }   
+            carry = 1;
+            
+        } else {
+            num[i] = sum + carry;
+             if(num[i] === 10) {
+                num[i] = 0;
+                carry = 1;
+            } else { 
+                carry = 0;
+            }
+        }
+    }
+    if(carry === 1) {
+        num.unshift(1);
+    }
+    return num;
+    
+};
