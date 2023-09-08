1b615bf94f6bda309655e9b4e81936aedd5f54a3
Minahil Hussain <minahilh21@gmail.com>
2023-09-08 12:42:26 +0500

    Time: 65 ms (83.51%), Space: 44.6 MB (92.28%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.ts
@@ -0,0 +1,15 @@
+function pivotIndex(nums: number[]): number {
+    let leftSum: number = 0;
+    let rightSum: number = 0;
+    for(let i = 0; i < nums.length; i++) {
+        if(i === 0) {
+            leftSum = 0;
+            for(let m = 1; m < nums.length; m++) rightSum += nums[m];  
+        } else {
+            leftSum += nums[i-1];
+            rightSum -= nums[i];
+        }
+        if (leftSum === rightSum) return i;
+    }
+    return -1;
+};
