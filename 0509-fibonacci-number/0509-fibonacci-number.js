e73726dcd15db67ddd4d0339580b9b5b12914486
Minahil Hussain <minahilh21@gmail.com>
2023-09-12 12:37:40 +0500

    Time: 55 ms (63.56%), Space: 41.9 MB (41.16%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.js
+++ 0509-fibonacci-number/0509-fibonacci-number.js
@@ -3,8 +3,16 @@
  * @return {number}
  */
 var fib = function(n) {
-    if(n == 1 || n == 0 ) return n;
+    if(n === 1 || n === 0 ) return n;
     else {
-        return fib(n - 1) + fib(n - 2);
+        let n1 = 1;
+        let n0 = 0;
+        let n2;
+        for (let i = 0; i <= n - 2; i++) {
+            n2 = n0 + n1;
+            n0 = n1;
+            n1 = n2;
+        }
+        return n2;
     }
 };
