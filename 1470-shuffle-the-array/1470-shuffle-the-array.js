ce30798193b6162286c5c873a7bfb6d5ade554de
Minahil Hussain <minahilh21@gmail.com>
2023-12-01 13:15:57 +0500

    Time: 60 ms (55.18%), Space: 43.9 MB (90.40%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.js
+++ 1470-shuffle-the-array/1470-shuffle-the-array.js
@@ -5,9 +5,6 @@
  */
 var shuffle = function(nums, n) {
     let newArr = [];
-    for(let i = 0; i< n ; i++) {
-        newArr.push(nums[i]);
-        newArr.push(nums[i+n]);
-    }
+    for(let i = 0; i< n ; i++) newArr.push(nums[i], nums[i+n]);
     return newArr;
 };
