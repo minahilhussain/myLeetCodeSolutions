05dae05dad2e4146ba9c2665ee95f9fe4d2f2aa0
Minahil Hussain <minahilh21@gmail.com>
2023-12-04 15:33:04 +0500

    Time: 44 ms (94.66%), Space: 42.1 MB (71.01%) - LeetHub

--- 2626-array-reduce-transformation/2626-array-reduce-transformation.js
+++ 2626-array-reduce-transformation/2626-array-reduce-transformation.js
@@ -5,10 +5,8 @@
  * @return {number}
  */
 var reduce = function(nums, fn, init) {
-    if (nums.length === 0) return init;
     for(let i = 0 ; i < nums.length; i++) {
-        let sum = fn(init, nums[i]);
-        init = sum;
+        init = fn(init, nums[i]);
     }
     return init;
 };
