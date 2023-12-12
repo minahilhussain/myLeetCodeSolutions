4b4c5ffb9ec310c7082ad53bce3e9d3e98026e89
Minahil Hussain <minahilh21@gmail.com>
2023-12-12 12:57:32 +0500

    Time: 54 ms (65.23%), Space: 42.4 MB (60.34%) - LeetHub

--- 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.js
+++ 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.js
@@ -5,6 +5,6 @@
 var maxProduct = function(nums1) {
   if (nums1.length === 2) return (nums1[0] - 1 ) * (nums1[1] - 1);
     
-  const nums = nums1.sort((a,b) => a - b);
-  return ((nums[nums.length-1] - 1 ) * (nums[nums.length -2] - 1));
+  const nums = nums1.sort((a,b) => b - a);
+  return ((nums[0] - 1 ) * (nums[1] - 1));
 };
