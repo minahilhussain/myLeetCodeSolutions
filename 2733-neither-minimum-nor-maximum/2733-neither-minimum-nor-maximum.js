e3ce1424f9e09fca3cc4703fb946ddc54c73f651
Minahil Hussain <minahilh21@gmail.com>
2024-08-22 21:41:33 +0500

    Time: 122 ms (92.55%), Space: 59.7 MB (36.17%) - LeetHub

--- 2733-neither-minimum-nor-maximum/2733-neither-minimum-nor-maximum.js
+++ 2733-neither-minimum-nor-maximum/2733-neither-minimum-nor-maximum.js
@@ -4,10 +4,18 @@
  */
 var findNonMinOrMax = function(nums) {
     if( nums.length < 3 ) return -1;
-    let min = Math.min(...nums);
-    let max = Math.max(...nums);
+    let min = nums[0];
+    let max = nums[0];
     for (let i = 0; i < nums.length; i++) {
-        if(nums[i] > min & nums[i] < max) return nums[i];
+        if(nums[i] < min) {
+            min = nums[i];
+        } 
+         if(nums[i] > max) {
+            max = nums[i];
+        } 
+    }
+    for (let i = 0; i < nums.length; i++) {
+        if(nums[i] !== min & nums[i] !== max) return nums[i];
     }
     return -1;   
 };
