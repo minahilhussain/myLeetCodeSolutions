715c0b59b46d5772441609b2dc09c75a89d06791
Minahil Hussain <minahilh21@gmail.com>
2024-08-28 23:14:10 +0500

    Time: 63 ms (54.41%), Space: 50.4 MB (16.18%) - LeetHub

--- 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.ts
+++ 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.ts
@@ -1,5 +1,3 @@
 function countOdds(low: number, high: number): number {
-    let count = ( high - low ) / 2;
-    if(low % 2 === 0 || high % 2 === 0) return count;
-    else return count + 1
+    return (low % 2 === 0 || high % 2 === 0) ? (( high - low ) / 2) : (( high - low ) / 2) + 1
 };
