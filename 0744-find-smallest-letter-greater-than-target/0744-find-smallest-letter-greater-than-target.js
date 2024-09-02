f489c21ca02f1004e4eff4bd27efc9994586bad5
Minahil Hussain <minahilh21@gmail.com>
2024-09-02 22:13:15 +0500

    Time: 44 ms (97.11%), Space: 51.5 MB (23.86%) - LeetHub

--- 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.js
+++ 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.js
@@ -6,7 +6,7 @@
 var nextGreatestLetter = function(letters, target) {
     const targetCode = target.charCodeAt();
     if (targetCode === 122) return letters[0];
-    for (let i = 0; i < letters.length; i++) {
+    for (let i = 0; i < letters.length; i++){
         if (letters[i].charCodeAt() > targetCode) return letters[i];
     }
     return letters[0]
