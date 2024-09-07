d786399a6fbf61e9ca17206be47099c9efa9bde4
Minahil Hussain <minahilh21@gmail.com>
2024-09-07 10:43:51 +0500

    Time: 53 ms (91.95%), Space: 51.7 MB (88.89%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.ts
+++ 0389-find-the-difference/0389-find-the-difference.ts
@@ -1,22 +1,11 @@
 function findTheDifference(strS: string, strT: string): string {
-    let mapS: { [key: string]: number } = {};
-    let mapT: { [key: string]: number } = {};
-    for (let i = 0; i < strS.length; i++) {
-       mapS[strS[i]] = mapS[strS[i]] + 1 || 1; 
-       mapT[strT[i]] = mapT[strT[i]] + 1 || 1;       
+    let sumS = 0, sumT = 0;
+
+    for (let i = 0; i < strS.length; i++){
+        sumS += strS[i].charCodeAt(0);
+        sumT += strT[i].charCodeAt(0);
     }
-    mapT[strT[strT.length - 1]] = (mapT[strT[strT.length - 1]] || 0) + 1;
-    
-    const keysS: string[] = Object.keys(mapS);
-    const keysT: string[] = Object.keys(mapT);
-    for (let key in mapT) {
-      if (mapT[key] !== mapS[key]) {
-        return key;
-      }
-    }
-    for (let key of keysT) {
-      if (!keysS.includes(key)) {
-        return key;
-      }
-    }
+    sumT += strT.charCodeAt(strT.length - 1);
+
+    return String.fromCharCode(sumT - sumS);
 };
