b82c6c0cc938a8d68131368b35e73fd4a92fd2e7
Minahil Hussain <minahilh21@gmail.com>
2024-09-10 21:23:19 +0500

    Time: 53 ms (76.25%), Space: 51.8 MB (7.28%) - LeetHub

---
+++ 3280-convert-date-to-binary/3280-convert-date-to-binary.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} date
+ * @return {string}
+ */
+var getBinary = (num) => {
+    let binary = '', rem;
+    while(num > 1) {
+       rem = num % 2;
+       num = Math.floor(num / 2);
+       binary = rem.toString() + binary;
+    }
+    return num + binary;
+}
+
+var convertDateToBinary = function(date) {
+   let dateArr =  date.split('-');
+   for (let i = 0; i < dateArr.length; i++) {
+       dateArr[i] = getBinary(Number(dateArr[i]));
+   }
+   const res = dateArr.join('-');
+   return res;
+ 
+};
+
