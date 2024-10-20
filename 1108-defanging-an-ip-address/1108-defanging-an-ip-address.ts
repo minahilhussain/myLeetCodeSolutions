c2fbbab9d6525fb70390c1f96ad0884b8fad6ea3
Minahil Hussain <minahilh21@gmail.com>
2024-10-20 22:16:35 +0500

    Time: 49 ms (85.15%), Space: 51.3 MB (29.70%) - LeetHub

--- 1108-defanging-an-ip-address/1108-defanging-an-ip-address.ts
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.ts
@@ -1,7 +1,3 @@
 function defangIPaddr(address: string): string {
-    let res = ''
-    for (let i = 0; i < address.length; i++) {
-        address[i] === '.' ? res+= '[.]' : res += address[i] 
-    }
-    return res;
+    return address.split('.').join('[.]');
 };
