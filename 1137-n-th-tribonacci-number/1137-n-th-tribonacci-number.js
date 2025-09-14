35235e853a68eb8cee100af71b844f7f11bf2a7f
Minahil Hussain <minahilh21@gmail.com>
2025-09-14 22:20:16 +0500

    Time: 0 ms (100%), Space: 54 MB (16.07%) - LeetHub

---
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+let memo = {};
+var tribonacci = function(n) {
+    if (n === 0) return 0;
+    if (n === 1 || n === 2) return 1;
+    if (memo[n]) return memo[n];
+    else memo[n] = tribonacci(n - 3) +  tribonacci(n - 2) + tribonacci(n - 1);
+    return memo[n];
+};
