e91b8487a6b5e82dde7acd3a3f4d940481ea0303
Minahil Hussain <minahilh21@gmail.com>
2025-09-14 22:14:12 +0500

    Time: 0 ms (100%), Space: 53.7 MB (37.28%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.js
+++ 0070-climbing-stairs/0070-climbing-stairs.js
@@ -6,6 +6,6 @@ let memo = {};
 var climbStairs = function(n) {
     if (n === 1 || n === 2) return n;
     if (memo[n]) return memo[n];
-    memo[n] = climbStairs(n - 1, memo) + climbStairs(n - 2, memo);
+    memo[n] = climbStairs(n - 1) + climbStairs(n - 2);
     return memo[n];
 };
