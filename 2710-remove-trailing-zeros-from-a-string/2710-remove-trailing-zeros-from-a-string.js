376d5ddb1271a825afa6a5876ebfb4949847e3a7
Minahil Hussain <minahilh21@gmail.com>
2025-09-22 14:17:35 +0500

    Time: 1 ms (78.7%), Space: 56.1 MB (69.44%) - LeetHub

--- 2710-remove-trailing-zeros-from-a-string/2710-remove-trailing-zeros-from-a-string.js
+++ 2710-remove-trailing-zeros-from-a-string/2710-remove-trailing-zeros-from-a-string.js
@@ -3,14 +3,13 @@
  * @return {string}
  */
 var removeTrailingZeros = function(num) {
-    if(num[ num.length-1 ] !== '0')  return num;
-    else {
-        let flag = 0;
-        for(let i = num.length - 1; i >= 0; i--) {
-            if (num[i] === '0') flag++;
-            else break;
+    let count = 0;
+    for (let i = num.length - 1; i > 0; i--) {
+        if (num[i] === "0"){
+            count++;
+        } else {
+            break;
         }
-        return num.substr(0, num.length - flag);
     }
-    
+    return num.slice(0, num.length - count);
 };
