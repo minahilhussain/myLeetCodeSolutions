fa64d292debcc271a29e63d6d2e1cd849c7b6e85
Minahil Hussain <minahilh21@gmail.com>
2025-10-22 20:50:13 -0700

    Time: 10 ms (66.67%), Space: 62.3 MB (48.15%) - LeetHub

--- 3768-check-if-digits-are-equal-in-string-after-operations-i/3768-check-if-digits-are-equal-in-string-after-operations-i.ts
+++ 3768-check-if-digits-are-equal-in-string-after-operations-i/3768-check-if-digits-are-equal-in-string-after-operations-i.ts
@@ -2,7 +2,7 @@ function hasSameDigits(s: string): boolean {
     while (s.length !== 2){ 
         let temp: string = '';
         for(let i = 0; i < s.length - 1; i++) {
-            temp += (parseInt(s[i]) + parseInt(s[i + 1])) % 10;
+            temp += (Number(s[i]) + Number(s[i + 1])) % 10;
         }
         s = temp;
     }
