d86f8f1a2e54700e8ee90125d591fd03f6d875f7
Minahil Hussain <minahilh21@gmail.com>
2025-10-24 04:09:27 -0700

    Time: 148 ms (79.33%), Space: 71.1 MB (50.63%) - LeetHub

---
+++ 0164-maximum-gap/0164-maximum-gap.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maximumGap = function(nums) {
+    if (nums.length === 1) return 0;
+    nums.sort((a,b) => a - b);
+    let difference = 0;
+    for (let i = 0; i < nums.length; i++) {
+        let dif = nums[i + 1] - nums[i];
+        if (dif > difference) difference = dif;
+    }
+    return difference;
+};
