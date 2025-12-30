b7ebd11de0609bd020b08593c8ebde791069ee6d
Minahil Hussain <minahilh21@gmail.com>
2025-12-31 00:37:12 +0500

    Time: 0 ms (100%), Space: 55.9 MB (63.91%) - LeetHub

---
+++ 0605-can-place-flowers/0605-can-place-flowers.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} flowerbed
+ * @param {number} n
+ * @return {boolean}
+ */
+var canPlaceFlowers = function(arr, n) {
+    if (n === 0) return true;
+    for (let i = 0; i < arr.length; i++) {
+        if (
+            arr[i] === 0 &&
+            (i === 0 || arr[i - 1] === 0) &&
+            (i === arr.length - 1 || arr[i + 1] === 0)
+        ) {
+            arr[i] = 1;
+            n--;
+            if (n === 0) return true;
+        }
+    }
+    return false;
+};
