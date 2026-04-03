5a2764a4a3ad438755410d4382dd521c5a9ca5b4
Minahil Hussain <minahilh21@gmail.com>
2026-04-03 12:01:08 +0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.ts
@@ -0,0 +1,7 @@
+function maximumProduct(nums: number[]): number {
+    nums.sort((a, b) => a - b);
+    const n = nums.length;
+    const option1 = nums[n - 1] * nums[n - 2] * nums[n - 3];
+    const option2 = nums[0] * nums[1] * nums[n - 1];
+    return Math.max(option1, option2);
+};
