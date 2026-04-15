0afcc08bce7071ecb2b71ecd201aec3e3d88a0f4
Minahil Hussain <minahilh21@gmail.com>
2026-04-15 11:08:57 +0500

    Time: 44 ms (11.01%), Space: 65.5 MB (12.85%) - LeetHub

---
+++ 1002-find-common-characters/1002-find-common-characters.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string[]} words
+ * @return {string[]}
+ */
+var commonChars = function (words) {
+    let objArr = [];
+    for (let i = 0; i < words.length; i++) {
+        let obj = {};
+        for (let j = 0; j < words[i].length; j++) {
+            obj[words[i][j]] = (obj[words[i][j]] || 0) + 1;
+        }
+        objArr.push(obj);
+    }
+    const result = [];
+    const base = objArr[0];
+    for (const char in base) {
+        const minCount = Math.min(
+            ...objArr.map(obj => obj[char] || 0)
+        );
+
+        for (let i = 0; i < minCount; i++) {
+            result.push(char);
+        }
+    }
+    return result;
+};
