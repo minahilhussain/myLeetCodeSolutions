5d61c633336cfc19b7957cf384886bbe4880271e
Minahil Hussain <minahilh21@gmail.com>
2026-04-30 12:33:23 +0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0263-ugly-number/0263-ugly-number.ts
@@ -0,0 +1,11 @@
+function isUgly(n: number): boolean {
+  if (n <= 0) return false;
+
+  for (let factor of [2, 3, 5]) {
+    while (n % factor === 0) {
+      n /= factor;
+    }
+  }
+
+  return n === 1;
+};
